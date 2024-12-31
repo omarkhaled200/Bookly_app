165e3a3ae74bf85760b68412e9ed4e592a244c25
omarkhaled200 <omarkhaled.6403@gmail.com>
2024-12-31 19:45:59 +0200

    Refactor in Splash View & Navigate to Home View

--- lib/Feature/Splash/peresentation/Views/widgets/Splash_View_Body.dart
+++ lib/Feature/Splash/peresentation/Views/widgets/Splash_View_Body.dart
@@ -1,6 +1,9 @@
 import 'package:bookly_app/Core/utlis/assets.dart';
 import 'package:bookly_app/Feature/Splash/peresentation/Views/widgets/sliding_animation.dart';
+import 'package:bookly_app/Feature/home/peresentation/Views/Home_view.dart';
+import 'package:bookly_app/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,13 +19,17 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration:const Duration(seconds: 2));
+    initSlidingAnimation();
 
-    slidinganimation =
-        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
+    navigateToHomeViews();
+  }
+
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
   }
 
   @override
@@ -38,5 +45,21 @@ class _SplashViewBodyState extends State<SplashViewBody>
         sliding_Animation(slidinganimation: slidinganimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(milliseconds:1500));
+
+    slidinganimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+    void navigateToHomeViews() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration:ktranstionduration);
+    });
   }
 }
