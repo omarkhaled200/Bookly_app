5cc99f1b8c2f4e0257962470c2358b65c9ee1864
omarkhaled200 <omarkhaled.6403@gmail.com>
2025-01-02 21:56:28 +0200

    Create Book Detials View file

---
+++ lib/Feature/home/peresentation/Views/Book_Detials_View.dart
@@ -0,0 +1,12 @@
+import 'package:bookly_app/Feature/home/peresentation/Views/widgets/Book_Detials_View_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetialsView extends StatelessWidget {
+  const BookDetialsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BookDetialsViewbody();
+  }
+}
+
---
+++ lib/Feature/home/peresentation/Views/widgets/Book_Detials_View_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookDetialsViewbody extends StatelessWidget {
+  const BookDetialsViewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
