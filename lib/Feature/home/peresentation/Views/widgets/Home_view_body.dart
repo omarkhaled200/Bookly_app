6adccf2f6006acec15e49bafe2d8b8d7c7721a85
omarkhaled200 <omarkhaled.6403@gmail.com>
2025-01-02 19:04:14 +0200

    Best seller item part 2

--- lib/Feature/home/peresentation/Views/widgets/Home_view_body.dart
+++ lib/Feature/home/peresentation/Views/widgets/Home_view_body.dart
@@ -3,15 +3,16 @@ import 'package:bookly_app/Core/utlis/assets.dart';
 import 'package:bookly_app/Feature/home/peresentation/Views/widgets/Custom_App_Bar.dart';
 import 'package:bookly_app/Feature/home/peresentation/Views/widgets/Featured_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  SafeArea(
+    return const SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: EdgeInsets.symmetric(horizontal: 30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -23,6 +24,9 @@ class HomeViewBody extends StatelessWidget {
             Text(
               'Best Seller',
               style: Styles.textStyle18,
+            ),
+            SizedBox(
+              height: 20,
             ),
             BestSellerlistViewitem(),
           ],
@@ -37,15 +41,29 @@ class BestSellerlistViewitem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return  Row(
       children: [
-        BestSellerlistviewitemImage(),
+      const  BestSellerlistviewitemImage(),
+      const  SizedBox(
+          width: 30,
+        ),
+        Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width*0.5,
+              child:const Text(
+                'Harry potter and the Goblet of Fire ',
+                style: Styles.textStyle20,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
 }
-
-
 
 class BestSellerlistviewitemImage extends StatelessWidget {
   const BestSellerlistviewitemImage({
