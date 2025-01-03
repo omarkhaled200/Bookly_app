f0450ab9ee8f875e7f293c6a40901a2286df2b06
omarkhaled200 <omarkhaled.6403@gmail.com>
2025-01-03 18:26:48 +0200

    Add Custom Scroll View to Book Detials

--- lib/Feature/home/peresentation/Views/widgets/Book_Detials_View_body.dart
+++ lib/Feature/home/peresentation/Views/widgets/Book_Detials_View_body.dart
@@ -13,48 +13,60 @@ class BookDetialsViewbody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          const CustomBookDetialsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.225),
-            child: const CustomBookImageitem(),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text('The Jungle Book', style: Styles.textStyle30),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              'Omar Khaled',
-              style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w500,
-              ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              children: [
+                const CustomBookDetialsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.225),
+                  child: const CustomBookImageitem(),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text('The Jungle Book', style: Styles.textStyle30),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    'Omar Khaled',
+                    style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const BooksAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 40,
+                  ),
+                ),
+                const Text('You Can also Like'),
+                const SizedBox(
+                  height: 8,
+                ),
+                const SimilarBookListView(),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 6,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-            const SizedBox(
-            height: 8,
-          ),
-          const
-           BooksAction(),
-             const SizedBox(
-            height: 40,
-          ),
-           Text('You Can also Like'),
-           SimilarBookListView(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
-
